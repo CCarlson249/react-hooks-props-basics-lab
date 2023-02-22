643c5c6c891843f065da8d315f48640dd3355e37
Clayton Carlson <claytonrc97@gmail.com>
2023-02-21 21:18:00 -0800

    lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Links from "../components/Links";
 
-function About() {
+
+
+function About(props) {
+  if(props.bio) {
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+       <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
-  );
-}
 
+  );
+} else {
+  return null;
+}
+}
 export default About;
